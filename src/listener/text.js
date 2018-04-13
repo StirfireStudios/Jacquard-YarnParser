4286655de0cfff45d64cd5d559552f8a28c7df3a
Brendan Ragan <lordmortis@gmail.com>
2018-04-13 17:00:45 +0800

    handle text statements

--- src/listener/text.js
+++ src/listener/text.js
@@ -5,14 +5,14 @@ const Location = require('../parser/location');
 
 function exit(ctx) {
 	const text = ctx.children.map(function(textNode) {
-		return textNode.children[0].toString();  
+		return textNode.toString();  
 	}).join("\n");
 	
 	this._statements.push(new TextStatement(text, Location.FromANTLRNode(ctx)));
 }
 
 function addToPrototype(prototype) {
-  prototype.exitLine_statement = exit;
+  prototype.exitText = exit;
 }
 
 module.exports = addToPrototype;
