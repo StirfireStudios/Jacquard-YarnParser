5fd38ef0de66fd4cad0ebd47fe565fd38fe92e96
Unknown <lordmortis@gmail.com>
2018-05-16 17:12:19 +0800

    Now parsing single line dialogue segments!

--- src/listener/statement.js
+++ src/listener/statement.js
@@ -1,45 +1,21 @@
 'use strict';
 
-const LineGroup = require('../statements/lineGroup');
-const Location = require('../parser/location');
+import LineGroup from '../statements/lineGroup';
+import Location from '../parser/location';
 const YarnParser = require('../antlr/YarnParser').YarnParser;
+import * as Util from './util';
 
 function enter(ctx) {
   if (this._statementGroup != null) return;
-
   const location = Location.FromANTLRNode(ctx);
   
-  const statementGroupParts = {
-		previousStatementGroup: this._statementGroup,
-		previousStatements: this._statements,
-    statements: [],
-    location: Location.FromANTLRNode(ctx),
-	}
-
-
-	this._statementGroup = statementGroupParts;
-	this._statements = statementGroupParts.statements;
+  Util.StatementGroup.Start.call(this, ctx);
 }
 
 function exit(ctx) {
   const lastNode = ctx.getChild(ctx.getChildCount() - 1);
   if (lastNode.symbol == null || lastNode.symbol.type !== YarnParser.NEWLINE) return;
-	const statementGroupParts = this._statementGroup;
-  this._statements = statementGroupParts.previousStatements;
-  this._statementGroup = statementGroupParts.previousStatementGroup;
-
-  if (statementGroupParts.statements.length == 0) {
-    return;
-  } else if (statementGroupParts.statements.length == 1) {
-    this._statements.push(statementGroupParts.statements[0]);
-  } else {
-    statementGroupParts.location.end.line = ctx.stop.line;
-    statementGroupParts.location.end.column = ctx.stop.column;
-    this._statements.push(new LineGroup(
-      statementGroupParts.statements,
-      statementGroupParts.location,
-    ));
-  }
+  Util.StatementGroup.End.call(this, ctx);
 }
 
 function addToPrototype(prototype) {
--- src/listener/util.js
+++ src/listener/util.js
@@ -1,12 +1,85 @@
 import DialogueSegment from '../statements/dialogueSegment';
+import LineGroup from '../statements/lineGroup';
+import Location from '../parser/location';
+
+// StatementGroup utils!
+function sgStart(ctx) {
+  const statementGroupParts = {
+		previousStatementGroup: this._statementGroup,
+		previousStatements: this._statements,
+    statements: [],
+    location: Location.FromANTLRNode(ctx),
+	}
+
+	this._statementGroup = statementGroupParts;
+	this._statements = statementGroupParts.statements;
+}
+
+function sgEnd(ctx) {
+  if (this._statementGroup == null) return;
+  const statementGroupParts = this._statementGroup;
+  this._statements = statementGroupParts.previousStatements;
+  this._statementGroup = statementGroupParts.previousStatementGroup;
+
+  if (statementGroupParts.statements.length == 0) {
+    return;
+  } else if (statementGroupParts.statements.length == 1) {
+    dsAddStatement.call(this, statementGroupParts.statements[0]);
+  } else {
+    statementGroupParts.location.end.line = ctx.stop.line;
+    statementGroupParts.location.end.column = ctx.stop.column;
+
+    dsAddStatement.call(this, new LineGroup(
+      statementGroupParts.statements,
+      statementGroupParts.location,
+    ));
+  }
+}
+
+const sgExternals = {
+  Start: sgStart,
+  End: sgEnd,
+}
 
 // Dialog segment utils!
+function dsStatement(statement) {
+  switch(statement.constructor.name) {
+    case "Blank":
+    case "Conditional":
+    case "DialogueSegment":
+    case "Evaluate": 
+    case "Function":
+    case "Link":
+    case "Option":
+    case "OptionGroup":
+    case "Shortcut":
+    case "ShortcutGroup":
+      return false;
+    case "LineGroup":
+    case "Text":
+      return true;
+    default: 
+      console.warn(`Unrecognized statement type: ${statement.constructor.name}`);
+      return false;
+  }
+}
+
 function dsAddStatement(statement) {
-  if (this._dialogSegment == null) {
-    this._dialogSegment = { identifier: null, statements: [] }
+  if (!dsStatement(statement)) {
+    dsFinish.call(this);
+    this._statements.push(statement);
+    return;
   }
 
-  this._dialogSegment.statement.push(statement);
+  if (!this._dialogSegmentRequiresBlankspace) {
+    dsFinish.call(this);
+  }
+
+  if (this._dialogSegment == null) {
+    this._dialogSegment = { identifier: null, statements: [] }
+  } 
+
+  this._dialogSegment.statements.push(statement);
 }
 
 function dsExist() { return this._dialogSegment != null; }
@@ -21,8 +94,13 @@ function dsFinish() {
   this._dialogSegment = null;
 }
 
-export const DialogueSegment = {
+
+const dsExternals = {
   AddStatement: dsAddStatement,
-  Finish: dsFinish,
   Exists: dsExist,
+  Finish: dsFinish,
 }
+
+export { dsExternals as DialogueSegment }
+
+export { sgExternals as StatementGroup }
